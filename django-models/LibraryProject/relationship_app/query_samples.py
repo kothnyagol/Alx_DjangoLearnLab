e47fdb5aa4c92c5346978bf59c510d0674e8e8d5
kothnyagol <kothnyagol@gmail.com>
2026-01-18 22:29:32 -0800

    Implement advanced Django model relationships with queries

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -3,9 +3,7 @@ from relationship_app.models import Author, Book, Library, Librarian
 
 # Query all books by a specific author
 def books_by_author(author_name):
-    author = Author.objects.get(name=author_name)
-    return author.books.all()
-
+    return Book.objects.filter(author__name=author_name)
 
 # List all books in a library
 def books_in_library(library_name):
