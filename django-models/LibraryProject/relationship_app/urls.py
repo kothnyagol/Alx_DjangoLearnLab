093520d7bcb49e9021084cb6cddaebae7c964b78
kothnyagol <kothnyagol@gmail.com>
2026-01-20 03:36:06 -0800

    Add authentication views using Django built-in login and UserCreationForm

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,17 +1,27 @@
-#!/usr/bin/ython3
+#!/usr/bin/python3
 from django.urls import path
-from .views import (
-    list_books,
-    LibraryDetailView,
-    register,
-    user_login,
-    user_logout
-)
+from django.contrib.auth import views as auth_views
+
+from . import views
+from .views import LibraryDetailView
 
 urlpatterns = [
-    path("books/", list_books, name="list_books"),
+    # Function-based view
+    path("books/", views.list_books, name="list_books"),
+
+    # Class-based view
     path("library/<int:pk>/", LibraryDetailView.as_view(), name="library_detail"),
-    path("register/", register, name="register"),
-    path("login/", user_login, name="login"),
-    path("logout/", user_logout, name="logout"),
+
+    # Authentication URLs (ALX REQUIRED)
+    path("register/", views.register, name="register"),
+    path(
+        "login/",
+        auth_views.LoginView.as_view(template_name="login.html"),
+        name="login",
+    ),
+    path(
+        "logout/",
+        auth_views.LogoutView.as_view(template_name="logout.html"),
+        name="logout",
+    ),
 ]
