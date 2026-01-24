202137c77f72c410b2c2407ca864f0848b3d3d47
kothnyagol <kothnyagol@gmail.com>
2026-01-24 06:02:52 -0800

    Add relationship_app with Author, Book, Library, Librarian models and query_samples.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -4,6 +4,7 @@ from .models import Book
 from .models import Library
 from django.contrib.auth import login, logout
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.decorators import user_passes_test
 
 # -----------------------------
 # Function-Based View: List all books
@@ -54,3 +55,33 @@ def login_view(request):
 def logout_view(request):
     logout(request)
     return render(request, 'relationship_app/logout.html')
+
+# -----------------------------
+# Role check functions
+# -----------------------------
+def is_admin(user):
+    return user.userprofile.role == 'Admin'
+
+def is_librarian(user):
+    return user.userprofile.role == 'Librarian'
+
+def is_member(user):
+    return user.userprofile.role == 'Member'
+
+
+# -----------------------------
+# Role-based views
+# -----------------------------
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
