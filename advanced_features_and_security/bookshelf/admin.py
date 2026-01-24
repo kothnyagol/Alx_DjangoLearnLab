df9b9070ebbea35deacae099668e0ff746b89441
kothnyagol <kothnyagol@gmail.com>
2026-01-24 03:26:24 -0800

    ALX Advanced Features & Security completed

--- advanced_features_and_security/bookshelf/admin.py
+++ advanced_features_and_security/bookshelf/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from .models import Book
 
+@admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'publication_year')  # Columns to display
-    search_fields = ('title', 'author')  # Add search box for title & author
-    list_filter = ('publication_year',)  # Filter by publication year
-admin.site.register(Book, BookAdmin)
+    list_display = ('title', 'author', 'publication_year')
+    search_fields = ('title', 'author')
+    list_filter = ('publication_year',)
--- advanced_features_and_security/users/admin.py
+++ advanced_features_and_security/users/admin.py
@@ -1,16 +1,15 @@
 from django.contrib import admin
-
-from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
 
+@admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
     model = CustomUser
     fieldsets = UserAdmin.fieldsets + (
-        ('Extra Info', {'fields': ('date_of_birth', 'profile_photo')}),
+        ('Extra Fields', {'fields': ('date_of_birth', 'profile_photo')}),
     )
     add_fieldsets = UserAdmin.add_fieldsets + (
-        ('Extra Info', {'fields': ('date_of_birth', 'profile_photo')}),
+        ('Extra Fields', {'fields': ('date_of_birth', 'profile_photo')}),
     )
-
-admin.site.register(CustomUser, CustomUserAdmin)
+    list_display = ('username', 'email', 'is_staff', 'is_superuser', 'date_of_birth')
+    search_fields = ('username', 'email')
