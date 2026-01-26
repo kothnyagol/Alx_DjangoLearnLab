a9302c90b06cabe3149b46f9051cbd16d03ea55b
kothnyagol <kothnyagol@gmail.com>
2026-01-26 05:16:21 -0800

    ALX: Implemented custom permissions and groups for Book model

--- advanced_features_and_security/LibraryProject/bookshelf/apps.py
+++ advanced_features_and_security/LibraryProject/bookshelf/apps.py
@@ -1,6 +1,22 @@
 from django.apps import AppConfig
-
+from django.contrib.auth.models import Group, Permission
+from django.contrib.contenttypes.models import ContentType
 
 class BookshelfConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'bookshelf'
+
+    def ready(self):
+        Book = self.get_model('Book')
+        content_type = ContentType.objects.get_for_model(Book)
+        permissions = Permission.objects.filter(content_type=content_type)
+
+        # Create groups if they don't exist
+        viewers, _ = Group.objects.get_or_create(name='Viewers')
+        editors, _ = Group.objects.get_or_create(name='Editors')
+        admins, _ = Group.objects.get_or_create(name='Admins')
+
+        # Assign permissions
+        viewers.permissions.set(permissions.filter(codename='can_view'))
+        editors.permissions.set(permissions.filter(codename__in=['can_create', 'can_edit']))
+        admins.permissions.set(permissions)
--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import permission_required
+from django.shortcuts import render, get_object_or_404
+from .models import Book
 
-# Create your views here.
+@permission_required('bookshelf.can_view', raise_exception=True)
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'bookshelf/book_list.html', {'books': books})
+
+@permission_required('bookshelf.can_create', raise_exception=True)
+def book_create(request):
+    # code to create a book
+    pass
+
+@permission_required('bookshelf.can_edit', raise_exception=True)
+def book_edit(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    # code to edit the book
+    pass
+
+@permission_required('bookshelf.can_delete', raise_exception=True)
+def book_delete(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    # code to delete the book
+    pass
