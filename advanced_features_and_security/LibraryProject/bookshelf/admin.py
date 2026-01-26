b2660889240c09ffe5726e9ebf398aa7ef8e7203
kothnyagol <kothnyagol@gmail.com>
2026-01-26 04:42:38 -0800

    Implemented Custom User Model with extra fields and admin integration

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,27 +1,26 @@
 from django.contrib import admin
-from .models import Book, CustomUser
 from django.contrib.auth.admin import UserAdmin
+from .models import Book, CustomUser
 
-# Register CustomUser
-@admin.register(CustomUser)
+# Custom UserAdmin
 class CustomUserAdmin(UserAdmin):
     model = CustomUser
-
     fieldsets = UserAdmin.fieldsets + (
         ('Extra Fields', {
             'fields': ('date_of_birth', 'profile_photo'),
         }),
     )
-
     add_fieldsets = UserAdmin.add_fieldsets + (
         ('Extra Fields', {
             'fields': ('date_of_birth', 'profile_photo'),
         }),
     )
-
     list_display = ('username', 'email', 'is_staff', 'date_of_birth')
 
-# Register Book
+# Register CustomUser explicitly (ALX wants this exact line)
+admin.site.register(CustomUser, CustomUserAdmin)
+
+# Book model admin
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'publication_year')
     search_fields = ('title', 'author')
