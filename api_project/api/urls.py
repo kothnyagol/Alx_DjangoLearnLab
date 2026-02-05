d042458323ab753524519f6bd0fa7f38708de40f
kothnyagol <kothnyagol@gmail.com>
2026-02-05 11:51:41 -0800

    Add token authentication and IsAuthenticated permission

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import BookList, BookViewSet
+from rest_framework.authtoken.views import obtain_auth_token
 
 # Create a router and register our viewset
 router = DefaultRouter()
@@ -9,6 +10,8 @@ router.register(r'books_all', BookViewSet, basename='book_all')
 urlpatterns = [
     # List-only view
     path('books/', BookList.as_view(), name='book-list'),
+
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
 
     # Include router URLs for all CRUD operations
     path('', include(router.urls)),
