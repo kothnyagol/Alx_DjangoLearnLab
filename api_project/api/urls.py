39db3418f0e1cee9f592aec03774b3e437545008
Nyagol Sharon Akoth <kothnyagol@gmail.com>
2026-02-21 01:32:16 -0800

    Deleted

--- api_project/api/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import BookListCreateAPIView
-
-urlpatterns = [
-    path('books/', BookListCreateAPIView.as_view(), name='book-list-create'),
-]
--- api_project/api/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from rest_framework import generics
-from .models import Book
-from .serializers import BookSerializer
-
-class BookListCreateAPIView(generics.ListCreateAPIView):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
